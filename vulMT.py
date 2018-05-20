0fbe674b5356f6da2698a766385da1c358ba87b0
BabyTiger <scarlett_guojia@hotmail.com>
2018-05-20 16:31:54 +0800

    [multi-process cmdline args support done] - can pass vulcmd, cl serial(1,2,3,4 ...) , tags from command line - note: -cmd must be quote with special " , incase mix with other param in cmdline
    [TODO]
    - Interface with autotriage cmd generate ...

--- vulMT.py
+++ vulMT.py
@@ -2,73 +2,95 @@
 
 ### Step-1 : multiprocess func run model
 from multiprocessing import *
-from multiprocessing.dummy import Pool as ThreadPool
 import subprocess as sub
 
+import re
 import time 
 
-# child-process func 
-def vulrun(tag):
-    DResu = {'tag':tag,'resu':'pass','webID':None}
+#############################
+#   child-process func      #
+#############################
 
-    cmdtemp="vulcan -v --eris --user jiag --dry-run --product=//sw/gpgpu/MachineLearning/cudnn_v7.1/eris/cudnn_r90_r384.vlcp --build cudnn --target-revision=cl-tot --target-os=Ubuntu16_04 --target-arch=ppc64le --tags={tag}".format_map(vars())
+def vulmtFunc(vulargs):
+    # cl: change-list / tags: notes of submit / Qbar: Queue log progress
+    cl, cmdtemp, tags, Qbar = vulargs
+    DResu = {'tags':tags,'resu':'pass','uuidLink':None}
 
-    print('=====> Start running {tag}'.format_map(vars()))
-    proc=sub.Popen(cmdtemp, bufsize=1, shell=True, stdout=sub.PIPE, stderr=sub.PIPE)
+    print('=====> Start running cl-{cl}'.format_map(vars()))
+    print('cmdline: ',cmdtemp.format_map(vars()))
+    proc=sub.Popen(cmdtemp.format_map(vars()), bufsize=1, shell=True, stdout=sub.PIPE, stderr=sub.PIPE)
     stdout,stderr = proc.communicate()
 
 # parse stdout/stderr
     strout=stdout.decode("utf-8")
     strerr=stderr.decode("utf-8")
     #queue , 
-    q.put(tag)
+    Qbar.put(tags)
 
-### func output/err collection
-# search for ERROR
-#### >>> stderr
-# b'[ERROR cuda.py:_get_eris_arch_targets:1851] unknown target architecture "ppc"\n    
-
+    # Filter error 
+    # b'[ERROR cuda.py:_get_eris_arch_targets:1851] unknown target architecture "ppc"\n    
     if 'ERROR' in strerr:
-        DResu['resu'] = 'failed'
-
-    DResu['webID'] = strout[0]
+        DResu['resu'] = 'failed :' + strerr
+        # if failed , no uuidLink at all... use "None" in default DResu
+    else:
+    #  TODO: can be an attri of vulcmd class 
+        start='ACCEPTED... Go to '
+        end=' to check its status.'
+    
+        DResu['uuidLink']=re.search('%s(.*)%s' % (start, end), strout).group(1)
 
     return (DResu.values())
 
-
 ##############################
 ###### main-process func ###### 
-m = Manager()
-q = m.Queue()
+##############################
+if __name__=="__main__":
+    ### cmdline arguments 
+    import argparse
+    parser = argparse.ArgumentParser(description='Auto Parse Eris uuid: test/build results')
 
-#################### p = Pool(cpu_count()) 
-p = ThreadPool(cpu_count())
+    # show command group 
+    parser.add_argument('-cl', action='store', dest='CLList', default=None, help='CL numbers to run, inform of 1,2,3 ')    
+    parser.add_argument('-cmd', action='store', dest='cmdline', default=None, help='full vul cmdline')    
+    parser.add_argument('-t', action='store', dest='tags', default=None, help='tags in cmdline')    
 
-taglists = ['Jia-11111','Jia-22222','Jia-33333','Jia-44444','Jia-55555','Jia-66666','Jia-77777','Jia-88888']
+    arglist=parser.parse_args()
+    print(parser.parse_args())
 
-### block mode 
-#result = p.map(vulrun,lists)
+    ### vulmtFunc paramter
+    #CLList = ['24068678','24068670','24068575','24068452','24068331','24068142','24068074','24068074','tot']
+    #CLList = ['hello','multi']
+    #cmdline="vulcan --keep-going -v --eris --user jiag --product=//sw/gpgpu/MachineLearning/cudnn_v7.1/eris/cudnn_r92_r396.vlcp --build cudnn_doc --target-os=Linux --target-arch=aarch64 --tag={tags} --target-revision=cl-{cl}"
+    #tags = 'MTJIA'
+   
+    CLList = arglist.CLList.split(',')
+    cmdline = re.sub('\"','',arglist.cmdline)
+    tags = arglist.tags
 
-# none block mode 
-result = p.map_async(vulrun,taglists)
-	
-### collect ALL process result in a Queue ? and summarize  
-# monitor loop
-while True:
-    if result.ready():
-        break
-    else:
-        size = q.qsize()
-        print(size)
-        time.sleep(3)
+    ##### multi-process param #####
+    m = Manager()
+    p = Pool(cpu_count()) 
+    q = m.Queue()
 
-outputs = result.get()
+    arglists = [ (cl,cmdline,tags,q) for cl in CLList ]
 
-###### slow mode
-###outputs = []
-###for t in taglists:
-###    outputs.append(vulrun(t))
-
-for o in outputs:
-    print(o)
-## Try timeit for *.py (multiprocess/for loop)
+    ### block mode 
+    #result = p.map(vulmtFunc,lists)
+    
+    # none block mode 
+    result = p.map_async(vulmtFunc,arglists)
+    	
+    ### collect ALL process result in a Queue ? and summarize  
+    # monitor loop
+    while True:
+        if result.ready():
+            break
+        else:
+            size = q.qsize()
+            print(size)
+            time.sleep(3)
+    
+    outputs = result.get()
+    
+    for o in outputs:
+        print(o)
